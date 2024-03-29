233e4519fb772b51a181a2f657fa20a275e5b1a9
Maryam Nasir <maryamnasir555@yahoo.com>
2024-03-29 16:40:30 +0500

    Modify year in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -57,7 +57,7 @@ const Footer = () => {
         </div>
         <div className="border bg-gray-20" />
         <p className="regular-14 w-full text-center text-gray-30">
-          2023 Hilink | All rights reserved
+          2024 Hilink | All rights reserved
         </p>
       </div>
     </footer>
